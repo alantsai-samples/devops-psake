008095bae9b8f60e197deced1f3f2a5b2d61d1f7
Alan Tsai <alan@alantsai.net>
2016-12-04 21:07:18 +0800

    增加bootstrap script

---
+++ src/BuildProject/build.ps1
@@ -0,0 +1,21 @@
+﻿# 如果psake module有存在，先把他反註解
+if (Get-Module -ListAvailable -Name psake) {
+	Remove-Module psake
+} 
+
+# 找到psake module並且註冊
+$psakeModulePath = (Get-ChildItem("..\packages\psake*\tools\psake.psm1")).FullName |
+					Sort-Object $_ | select -Last 1
+
+if(Test-Path $psakeModulePath){
+	Import-Module $psakeModulePath
+}else{
+	Write-Host "找不到psake module，請確認好nuget package有restore完成"
+	return
+}
+
+# 執行psake
+Invoke-psake -buildFile .\default.ps1 -taskList Test `
+			 -properties @{
+				"testMsg"="測試訊息"
+			 }
