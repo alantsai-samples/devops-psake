cb216803ac3ab70d11dc27c6079d4db594d07beb
Alan Tsai <alan@alantsai.net>
2016-12-06 06:45:01 +0800

    把.build資料夾檔案都建立出來

--- src/BuildProject/default.ps1
+++ src/BuildProject/default.ps1
@@ -5,14 +5,29 @@
 
 	$solutionDirectory = (Get-Item $solutionFile).DirectoryName
 	$buildDirectory = "$solutionDirectory\.build"
+	$buildTempDirectory = "$buildDirectory\temp"
+	$buildTestResultDirectory = "$buildDirectory\testResult"
+	$buildTestCoverageDirectory = "$buildDirectory\testCoverage"
+	$buildArtifactDirectory = "$buildDirectory\artifact"
 }
 
 task default -depends Test
 
 task Init -depends Clean -description "初始化建制所需要的設定"{
-	
 	Write-Host "建立建制結果的資料夾 $buildDirectory"
 	New-Item $buildDirectory -ItemType Directory | Out-Null
+
+	Write-Host "建立建制結果裡面的Temp資料夾 $buildTempDirectory"
+	New-Item $buildTempDirectory -ItemType Directory | Out-Null
+
+	Write-Host "建立建制結果裡面的TestResult資料夾 $buildTestResultDirectory"
+	New-Item $buildTestResultDirectory -ItemType Directory | Out-Null
+
+	Write-Host "建立建制結果裡面的TestCoverage資料夾 $buildTestCoverageDirectory"
+	New-Item $buildTestCoverageDirectory -ItemType Directory | Out-Null
+
+	Write-Host "建立建制結果裡面的Artifact資料夾 $buildArtifactDirectory"
+	New-Item $buildArtifactDirectory -ItemType Directory | Out-Null
 }
 
 task Test -depends Compile, Clean -description "執行Test" { 
@@ -24,5 +39,11 @@ task Compile -depends Clean, Init -description "編譯程式碼" {
 }
 
 task Clean -description "刪除上次編譯遺留下來的內容"{ 
+
+	if(Test-Path $buildDirectory){
+		Write-Host "清除上次編譯的結果 $buildDirectory"
+		Remove-Item $buildDirectory -Recurse -Force
+	}
+
 	Write-Host $cleanMsg
 }
