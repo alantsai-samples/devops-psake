3a0b51832280c4673061192ba9dd1579a616fc77
Alan Tsai <alan@alantsai.net>
2016-12-06 06:21:52 +0800

    增加init的task

--- src/BuildProject/default.ps1
+++ src/BuildProject/default.ps1
@@ -2,15 +2,24 @@
 	$testMsg = "Executed Test !"
 	$compileMsg = "Executed Compile !"
 	$cleanMsg = "Executed Clean !"
+
+	$solutionDirectory = (Get-Item $solutionFile).DirectoryName
+	$buildDirectory = "$solutionDirectory\.build"
 }
 
 task default -depends Test
+
+task Init -depends Clean -description "初始化建制所需要的設定"{
+	
+	Write-Host "建立建制結果的資料夾 $buildDirectory"
+	New-Item $buildDirectory -ItemType Directory | Out-Null
+}
 
 task Test -depends Compile, Clean -description "執行Test" { 
 	Write-Host $testMsg
 }
 
-task Compile -depends Clean -description "編譯程式碼" { 
+task Compile -depends Clean, Init -description "編譯程式碼" { 
 	Write-Host $compileMsg
 }
 
