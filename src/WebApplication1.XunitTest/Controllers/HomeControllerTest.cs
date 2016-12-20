abd908a561265b0c6a0c6cc7959c295a617a4f82
Alan Tsai <alan@alantsai.net>
2016-12-21 00:00:53 +0800

    增加xunit測試的專案

---
+++ src/WebApplication1.XunitTest/Controllers/HomeControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using WebApplication1.Controllers;
+using Xunit;
+
+namespace WebApplication1.XunitTest.Controllers
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void Contact()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Contact() as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+    }
+}
