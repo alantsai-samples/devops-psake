f790b93404849930cc0fe38691648b181b5d2766
Alan Tsai <alan@alantsai.net>
2017-07-29 21:10:14 +0800

    [重構]修正取得檔案路徑的方式 - 避免執行script位置不同導致路徑問題

--- src/BuildProject/build.ps1
+++ src/BuildProject/build.ps1
@@ -1,11 +1,25 @@
-﻿# 如果psake module有存在，先把他反註解
+﻿# 目前script的位置
+$sciptCurrentDir = Split-Path $MyInvocation.MyCommand.Path -Parent
+
+# build script 位置
+$buildFile = Join-Path $sciptCurrentDir .\default.ps1
+
+# solution檔案位置
+$solutionFile = (Get-ChildItem(Join-Path $sciptCurrentDir "..\*.sln")).FullName |
+					Sort-Object $_ | select -Last 1
+
+# 找到和 build一起的 psake module
+$psakeModulePath = (Get-ChildItem( Join-Path $sciptCurrentDir "..\packages\psake*\tools\psake.psm1")).FullName |
+						Sort-Object $_ | select -Last 1
+
+
+# 如果psake module有存在，先把他反註解
 if (Get-Module -ListAvailable -Name psake) {
 	Remove-Module psake
 } 
 
 # 找到psake module並且註冊
-$psakeModulePath = (Get-ChildItem("..\packages\psake*\tools\psake.psm1")).FullName |
-					Sort-Object $_ | select -Last 1
+$psakeModulePath = $psakeModulePath
 
 if(Test-Path $psakeModulePath){
 	Import-Module $psakeModulePath
@@ -15,11 +29,10 @@ if(Test-Path $psakeModulePath){
 }
 
 # 執行psake
-Invoke-psake -buildFile .\default.ps1 -taskList Test `
+Invoke-psake -buildFile $buildFile -taskList Test `
 			 -framework "4.6" `
 			 -parameters @{
-				"solutionFile" = (Get-ChildItem("..\*.sln")).FullName |
-					Sort-Object $_ | select -Last 1
+				"solutionFile" = $solutionFile
 			 }`
 			 -properties @{
 				"testMsg"="測試訊息"
